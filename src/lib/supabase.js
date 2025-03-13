59bfdc1960031925baded47fd8055e1903f2b588
onggi <onggisanjaya@gmail.com>
2025-03-13 15:57:41 +0700

    perbaikan .env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,11 +4,8 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error(
-    "Missing Supabase URL or Anon Key. Check your .env.local file."
-  );
+  throw new Error("Missing Supabase URL or Anon Key");
 }
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
 export default supabase;
